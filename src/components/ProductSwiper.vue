eae1039c1ee78b7139574e35d63b3deef048fb00
“iris <chidy76071919@hotmail.com>
2024-03-24 15:50:09 +0800

    fix:style scoped

--- src/components/ProductSwiper.vue
+++ src/components/ProductSwiper.vue
@@ -59,7 +59,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .swiper {
   width: 100%;
   height: 100%;
--- src/components/UserComment.vue
+++ src/components/UserComment.vue
@@ -74,7 +74,7 @@ export default {
   }
 }
 </script>
-<style>
+<style scoped>
 .swiper {
   width: 100%;
   height: 100%;
