13f3d5bce8112a90c02570e70caa2824ce10a2e8
“iris <chidy76071919@hotmail.com>
2024-03-25 19:32:09 +0800

    fix: remove AOS

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -55,7 +55,7 @@
         </div>
       </div>
     </div>
-    <div class="row mt-5" data-aos="zoom-in">
+    <div class="row mt-5">
       <div class="col-md-3 mt-md-4">
         <div class="card border-0 mb-4 position-relative text-center">
           <i style="font-size: 50px;" class="bi bi-flower2"></i>
@@ -122,27 +122,27 @@
     </div>
   </div>
   <div class="container my-5">
-    <div class="row" data-aos="fade-left">
+    <div class="row">
       <div class="col-md-6">
         <img
           src="https://storage.googleapis.com/vue-course-api.appspot.com/iris831206/1710771948772.jpg?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=jAmsuCOGxNvS2gY5n67YJjDViwiiBKUygsWfynXiXwq3tGVEk8EyfpplNW54mWi%2FR9PgSKVsC5SrsQ7kXbyjSmhiJlL0EdAgSrwLQvrEhNTgaKZa24jQJO56GMSVLH4NJQwsSkA36e3wvEupT8WRSWTHNSk87Vsi6DRFKeLod1dNK95l%2B%2FmzwKSoLdJJ2NxG4kNfLb5ag6%2B8D7AMTQvAyw8Nc8WgukLBNlUZ1K1jBC2sWSTAhv28lh1D9a3CSVEgcmssSOf7cD8aN7GjgRz2KYEfyQztxQZ1S5htxhqZPJWgbRX3siVz3nN40EoiMMuupRa%2BuWPeANKFiCEnVr8z8Q%3D%3D"
           style="object-fit:cover" alt="" class="img-fluid">
       </div>
       <div class="col-md-4 m-auto text-center">
-        <h4 class="mt-4" data-aos="fade-left">療癒身心</h4>
+        <h4 class="mt-4">療癒身心</h4>
         <p class="text-muted">
           每次沐浴都是一次環繞自己的美麗禮讚
         </p>
       </div>
     </div>
-    <div class="row flex-row-reverse justify-content-between mt-4" data-aos="fade-right">
+    <div class="row flex-row-reverse justify-content-between mt-4">
       <div class="col-md-6">
         <img
           src="https://storage.googleapis.com/vue-course-api.appspot.com/iris831206/1710772108160.jpg?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=HwA3eT1w1fxwBT%2BbItOZodTuWN3YxXMYskA0xfZvCrFLggfRFjQG432FY8ks6kmmQHtuL48DBQ0YqWKg1zd4nWl6DSfCZq4yUbSc4q0Z6xYZR7WQBFSmEmLers2PbvqVKOUoO%2FHxYNHifUZilkDFUVXJlegWtUYHweHbYX%2BDy5NH9KDmira5qy6UhWixOwVtKxU9ktqpMiFaE9n1%2FT6gor8SG8JJnSuvH%2FSJColkX93gm7AI%2BJnQOvjuhpAEmjE2z8%2FF41W3f5m8dRh3PnZXizOZqSMwSvhD%2BI%2BwQq0oqt5YktVE2sacj5L5DacZBRSeMI4yJgm0q6oat3P92IAIUQ%3D%3D"
           style="object-fit:cover" alt="" class="img-fluid">
       </div>
       <div class="col-md-4 m-auto text-center">
-        <h4 class="mt-4" data-aos="fade-right">享受生活</h4>
+        <h4 class="mt-4">享受生活</h4>
         <p class="text-muted">
           為您帶來無與倫比的沐浴體驗
         </p>
